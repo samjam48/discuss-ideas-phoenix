c080d209bdd9ca0431bbe25d435ce5b45b240d69
Sam Harris <sam@jam-bc.com>
2019-11-12 21:22:05 +0000

    remove template phoenix pages

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -16,6 +16,11 @@ defmodule Discuss.TopicController do
   #   %{"topic" => topic} = params
   # end
   def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
 
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> render conn, "new.html", changeset: changeset
+    end
   end
 end
