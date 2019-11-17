724b26bd0aad5e8103c6c1ffa2249018e13b9087
Sam Harris <sam@jam-bc.com>
2019-11-17 13:57:09 +0000

    crud implemented for topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,15 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+
+
   def new(conn, _params) do
     # struct = %Topic{}
     # params = %{}
@@ -12,6 +21,8 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
 
+
+
   # def create(conn, params) do
   #   %{"topic" => topic} = params
   # end
@@ -19,8 +30,60 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> render conn, "new.html", changeset: changeset
     end
+  end
+
+
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    # fetch topic out of database and then start editing it
+
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+    # changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+  end
+
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+
+
+    # case Repo.delete(topic) do
+    #   {:ok, _topic} ->
+    #     conn
+    #     |> put_flash(:info, "Topic Deleted")
+    #     |> redirect(to: topic_path(conn, :index))
+    #   {:error, changeset} ->
+    #     conn
+    #     |> put_flash(:info, "Could not delete!!!")
+    #     |> redirect(to: topic_path(conn, :index))
+    # end
+
   end
 end
